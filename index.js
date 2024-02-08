c343679aaa1ced23f7ae210bd35101dcdbc30878
Andrea <andrea.raneri94@gmail.com>
2024-02-08 15:01:29 +0100

    Commit consegna

---
+++ index.js
@@ -0,0 +1,12 @@
+Consegna 08/02/2024
+
+1. React JS è una libreria java script a supporto della creazione di pagine web dinamiche
+2. FALSO
+3. Molte informazioni utili, come ad esempio l’elenco di tutte le dipendenze richieste dall’applicazione
+4. Falso
+5. npx create-react-app test
+6. Un blocco di logica/contenuto riutilizzabile all’interno dell’applicazione
+7. Falso
+8.Vero
+9.Falso
+10.Sono proprietà contenute in un oggetto accessibile attraverso i parametri della funzione
